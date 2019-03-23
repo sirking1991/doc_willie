3805c0f43d2bcc49eb09543275c767f40360ff15
sherwin <sirking1991@gmail.com>
2019-03-24 00:24:16 +0800

    Implemented horizontal ListView for video playlists

--- lib/videos.dart
+++ lib/videos.dart
@@ -20,7 +20,8 @@ class _VideosState extends State<Videos> {
       mainAxisAlignment: MainAxisAlignment.start,
       mainAxisSize: MainAxisSize.max,
       children: <Widget>[
-        Container(height: 200, margin: EdgeInsets.only(top: 5), child: VideoPlaylist()),
+        Container(height: 200, color: Colors.green, margin: EdgeInsets.only(top: 3), child: VideoPlaylist()),
+        Container(color: Colors.blue, margin: EdgeInsets.only(top: 3), child: PlaylistVideos()),
       ],
     );
   }
@@ -153,119 +154,23 @@ class _VideoPlaylistState extends State<VideoPlaylist> {
   }
 }
 
-/*
-class DataRetriever {
-  final String YT_API_KEY = "AIzaSyBFlY_O8wo9YlArb-35P8zZ_zVbq-rSiJU";
-  Future<List<Item>> fetchUsersFromGitHub() async {
-    Playlists playlists;
-    final String url =
-        'https://www.googleapis.com/youtube/v3/playlistItems?key='+YT_API_KEY+'&part=snippet,contentDetails&playlistId=PLqxfhR260JI7V4ELdNqtIFzCqi6Rtjt7x&maxResults=50';
 
-    http.Response response = await http
-        .get(Uri.encodeFull(url), headers: {"Accept": "application/json"});
-    if (200 == response.statusCode) {
-        playlists = Playlists.fromJson(json.decode(response.body));
-        return playlists.items;
-    } else {
-      throw Exception('Failed to load playlist');
-    }
-  }
-}
-*/
-
-/*
-
-  final TextEditingController _textController = new TextEditingController();
-  List<Item> items = [];
-  var youtube = new FlutterYoutube();
-
-      children: <Widget>[
-        SizedBox.fromSize(
-          size: Size(0, 10),
-        ),
-        Container(
-          margin: const EdgeInsets.symmetric(horizontal: 12.0),
-          padding: const EdgeInsets.all(10),
-          decoration: BoxDecoration(
-              border: Border.all(width: 2.0, color: Colors.blue),
-              borderRadius: BorderRadius.circular(10)),
-          child: new TextField(
-            textInputAction: TextInputAction.search,
-            controller: _textController,
-            onSubmitted: (String text) => print(text),
-            decoration:
-                new InputDecoration.collapsed(hintText: "Search videos"),
-          ),
-        ),
-        SizedBox.fromSize(
-          size: Size(0, 10),
-        ),
-        Expanded(
-          child: VideoItems()
-        ),
-      ],
-*/
-
-/*
-class VideoItems extends StatefulWidget {
+class PlaylistVideos extends StatefulWidget {
   @override
-  _VideoItemsState createState() => _VideoItemsState();
+  _PlaylistVideosState createState() => _PlaylistVideosState();
 }
 
-class _VideoItemsState extends State<VideoItems> {
-  List<Item> items;
-  items =  DataRetriever();
-
+class _PlaylistVideosState extends State<PlaylistVideos> {
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: items.length,
-      itemBuilder: (BuildContext context, int index) {
-        return GestureDetector(
-          onTap: () {
-            FlutterYoutube.playYoutubeVideoById(
-                apiKey: YT_API_KEY,
-                videoId: items[index].snippet.resourceId.videoId,
-                autoPlay: false,
-                fullScreen: false //default false
-            );
-          },
-          child: Card(
-            child: Padding(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: <Widget>[
-                  FadeInImage(
-                    width: 100,
-                    placeholder:
-                    new AssetImage('images/yt_placeholder.png'),
-                    image: new CachedNetworkImageProvider(items[index]
-                        .snippet
-                        .thumbnails
-                        .thumbnailsDefault
-                        .url),
-                    fit: BoxFit.contain,
-                    alignment: Alignment.center,
-                    fadeInDuration: new Duration(milliseconds: 500),
-                    fadeInCurve: Curves.linear,
-                  ),
-                  SizedBox(width: 10),
-                  Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: <Widget>[
-                          Text(items[index].snippet.title),
-                        ],
-                      )),
-                ],
-              ),
-              padding: EdgeInsets.all(10),
-            ),
-          ),
-        );
-      },
+    return ListView(
+      children: <Widget>[
+        Card(child: Text("Item one"),),
+        Card(child: Text("Item two"),),
+        Card(child: Text("Item three"),),
+        Card(child: Text("Item four"),),
+        Card(child: Text("Item five"),),
+      ],
     );
   }
 }
-*/
